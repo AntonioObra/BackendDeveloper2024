5bdccb37471f1b4370f6d7c36517b4876db962c3
AntonioObra <antonio@obradovic.dev>
2024-12-09 18:14:48 +0100

    init laravel

---
+++ php/obrazac.php
@@ -0,0 +1,35 @@
+<style>
+table,
+th {
+    border: 1px solid black;
+}
+</style>
+
+<form action="">
+    <table>
+        <tbody>
+            <tr>
+                <th>Ime:</th>
+                <th><input type="text"></th>
+            </tr>
+            <tr>
+                <th>Prezime:</th>
+                <th><input type="text"></th>
+            </tr>
+            <tr>
+                <th>Email:</th>
+                <th><input type="text"></th>
+            </tr>
+            <tr>
+                <th>Lozinka:</th>
+                <th><input type="text"></th>
+            </tr>
+
+            <tr>
+                <th colspan="2">
+                    <button type="submit">Posalji</button>
+                </th>
+            </tr>
+        </tbody>
+    </table>
+</form>
